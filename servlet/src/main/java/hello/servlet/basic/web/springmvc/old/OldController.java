746752c227c40acf02538dd3c1d1e11552ea3fa6
seunglo <devseung9056@gmail.com>
2022-05-07 02:28:03 +0900

    실습을 위한 레거시 핸들러를 추가해라

---
+++ servlet/src/main/java/hello/servlet/basic/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.basic.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 스프링 빈의 이름이 핸들러 매핑의 키값이 된고 동시에 URL 매핑 경로가 된다
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
